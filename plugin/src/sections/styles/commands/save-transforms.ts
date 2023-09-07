30cebc3f02ebbd64cf709ca29ce2e70ee1abe836
gossi <gossi@users.noreply.github.com>
2023-09-07 17:14:06 +0200

    Fix UI Glitches in the Styles Section

--- plugin/src/sections/styles/commands/save-transforms.ts
+++ plugin/src/sections/styles/commands/save-transforms.ts
@@ -2,7 +2,10 @@ import Command from '../../../infrastructure/command';
 import { CommandName, type Commands } from '@theemo-figma/core/styles/commands';
 import { readConfig, storeConfig } from '../store';
 import { VariableConfig } from '@theemo-figma/core/styles';
-import { getResolvedValue } from '../../../variables';
+import { findVariableFromAlias, getResolvedValue } from '../../../variables';
+import { findVariableAlias, hasBoundVariable } from '../../../styles';
+import { updatePaintStyle } from '../referencer';
+import { EventName } from '@theemo-figma/core/styles/events';
 
 export default class SaveTransformsCommand extends Command {
   NAME = CommandName.SaveTransforms;
@@ -10,20 +13,42 @@ export default class SaveTransformsCommand extends Command {
   execute(data: Commands[CommandName.SaveTransforms]) {
     const config = readConfig();
     const existing = config.variables.find(varConf => varConf.variableId === data.variableId && varConf.modeId === data.modeId);
+    const referenceAlias = this.findReference(data);
 
     if (existing) {
       existing.transforms = data.transforms;
     } else {
-      const reference = this.findReference(data);
       const conf: VariableConfig = {
         ...data,
-        referenceId: reference.id
+        referenceId: referenceAlias.id
       };
       config.variables.push(conf);
       this.unlinkVariable(data);
     }
 
     storeConfig(config);
+
+    // attempt an update
+    const reference = figma.variables.getVariableById(referenceAlias.id);
+
+    if (reference) {
+      const consumingStyles = figma.getLocalPaintStyles().filter(style => {
+        if (hasBoundVariable(style)) {
+          const alias = findVariableAlias(style);
+          const variable = alias && findVariableFromAlias(alias);
+
+          return variable && variable.id === reference.id;
+        }
+        return false;
+      });
+
+      for (const styleToUpdate of consumingStyles) {
+        updatePaintStyle({ style: styleToUpdate, id: styleToUpdate.id, name: styleToUpdate.name });
+      }
+    }
+
+    // update config in UI
+    this.emitter.sendEvent(EventName.ConfigArrived, config);
   }
 
   findReference(data: Commands[CommandName.SaveTransforms]): VariableAlias {
@@ -35,6 +60,5 @@ export default class SaveTransformsCommand extends Command {
     const variable = figma.variables.getVariableById(data.variableId) as Variable;
     const value = getResolvedValue(variable, data.modeId);
     variable.setValueForMode(data.modeId, value);
-
   }
 }
